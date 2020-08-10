b85af68077d8247069497263dacd4f8ecf77284e
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-08-10 15:11:59 -0400

    added requirments to access edit and delete paths

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def show
         set_user
@@ -40,6 +42,13 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and all associated articles have been deleted"
+        redirect_to root_path
+    end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
@@ -49,6 +58,11 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
     
-
+    def require_same_user
+        if current_user != @user
+            flash[:alert] = "You can only edit or delete your own account"
+            redirect_to @user
+        end
+    end
 
 end
