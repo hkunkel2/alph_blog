6589b20fa763192a428262673556544ba2555157
Harrison Kukel <HarrisonJamesKunkel@gmail.com>
2020-08-10 11:57:26 -0400

    add pagination to articles listing page, users listing page, and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,13 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
-        @articles = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
+
+    def index
+        @users = User.paginate(page: params[:page], per_page: 5) 
+    end
+
     def new
         @user = User.new
     end
@@ -17,7 +22,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
@@ -27,7 +32,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
         if @user.save
             flash[:notice] = "welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'new'
         end
